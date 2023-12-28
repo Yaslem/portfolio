22092ee1c0af3b8ab6d1612c4e62c74f89ce0a70
Yeslem <yeslem.alshanqyti@gmail.com>
2023-12-28 17:07:32 +0300

    تعديل على ملفات المشروع

--- app/layout.js
+++ app/layout.js
@@ -6,33 +6,33 @@ axios.defaults.baseURL = process.env.BASE_URL_API;
 
 export const metadata = {
   title: {
-    template: "%s | يسلم أحمد ناجم",
-    default: "يسلم أحمد ناجم"
+    template: `%s | ${process.env.SITE_TITLE}`,
+    default: process.env.SITE_TITLE
   },
   description: 'يسلم أحمد ناجم، مطور مواقع الويب، وطالب شرعي. قمت ببرمجة العديد من المواقع، أسعى لبرمجة وابتكار حلول تقنية تساعد في إثراء المجتمع التقني.',
   category: 'programming',
-  generator: 'Nextjs',
-  applicationName: 'يسلم أحمد ناجم',
+  generator: process.env.SITE_TITLE,
+  applicationName: process.env.SITE_TITLE,
   referrer: 'origin-when-cross-origin',
-  keywords: ['يسلم أحمد ناجم', 'يسلم الشنقيطي', 'البرمجة يسلم', 'Yeslem Ahmed Najem'],
-  authors: [{ name: 'يسلم أحمد ناجم' }, { name: 'Yeslem Ahmed Najem', url: 'https://nextjs.org' }],
-  creator: 'يسلم أحمد ناجم',
-  publisher: 'يسلم أحمد ناجم',
+  keywords: [process.env.SITE_TITLE, 'يسلم الشنقيطي', 'البرمجة يسلم', 'Yeslem Ahmed Najem'],
+  authors: [{ name: process.env.SITE_TITLE }, { name: 'Yeslem Ahmed Najem', url: 'https://yeslem.dev' }],
+  creator: process.env.SITE_TITLE,
+  publisher: process.env.SITE_TITLE,
   formatDetection: {
     email: true,
     address: true,
     telephone: true,
   },
-  metadataBase: new URL('https://acme.com'),
+  metadataBase: new URL('https://yeslem.dev'),
   alternates: {
     canonical: '/',
   },
   openGraph: {
     images: '/favicon.png',
-    title: 'يسلم أحمد ناجم',
-    description: 'يسلم أحمد ناجم، مطور مواقع الويب، وطالب شرعي. قمت ببرمجة العديد من المواقع، أسعى لبرمجة وابتكار حلول تقنية تساعد في إثراء المجتمع التقني.',
-    url: 'https://nextjs.org',
-    siteName: 'يسلم أحمد ناجم',
+    title: process.env.SITE_TITLE,
+    description: `${process.env.SITE_TITLE}، مطور مواقع الويب، وطالب علم شرعي. قمت ببرمجة العديد من المواقع، أسعى لبرمجة وابتكار حلول تقنية تساعد في إثراء المجتمع التقني.`,
+    url: 'https://yeslem.dev',
+    siteName: process.env.SITE_TITLE,
     locale: 'ar_MR',
     type: 'website',
   },
